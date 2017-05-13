c01a608489e62010c927ef02b2f0bd2d74f3004a
Sebastian Solnica <ssolnica@gmail.com>
2017-05-13 17:44:17 +0200

    registry events continued

--- wtrace/Handlers/RegistryTraceEventHandler.cs
+++ wtrace/Handlers/RegistryTraceEventHandler.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.Diagnostics.Tracing.Parsers;
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
 using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using Microsoft.Diagnostics.Tracing;
 
 namespace LowLevelDesign.WinTrace.Handlers
 {
@@ -11,7 +9,7 @@ namespace LowLevelDesign.WinTrace.Handlers
     {
         private readonly ITraceOutput output;
         private readonly int pid;
-        //private readonly Dictionary<ulong, string> registryHandleToKeyNameMap = new Dictionary<ulong, string>();
+        private readonly Dictionary<ulong, string> registryHandleToKeyNameMap = new Dictionary<ulong, string>();
 
         public RegistryTraceEventHandler(int pid, ITraceOutput output)
         {
@@ -28,9 +26,9 @@ namespace LowLevelDesign.WinTrace.Handlers
         public void SubscribeToEvents(TraceEventParser parser)
         {
             var kernel = (KernelTraceEventParser)parser;
-            kernel.RegistryClose += HandleRegistryTraceData;
+            kernel.RegistryClose += HandleRegistryCloseTraceData;
             kernel.RegistryCreate += HandleRegistryTraceData;
-            kernel.RegistryDelete += HandleRegistryTraceData;
+            kernel.RegistryDelete += HandleRegistryCloseTraceData;
             kernel.RegistryDeleteValue += HandleRegistryTraceData;
             kernel.RegistryEnumerateKey += HandleRegistryTraceData;
             kernel.RegistryEnumerateValueKey += HandleRegistryTraceData;
@@ -48,19 +46,45 @@ namespace LowLevelDesign.WinTrace.Handlers
             kernel.RegistryVirtualize += HandleRegistryTraceData;
         }
 
+        private void HandleRegistryCloseTraceData(RegistryTraceData data)
+        {
+            if (data.ProcessID == pid) {
+                ulong keyHandle = data.KeyHandle;
+                string keyName;
+                if (!registryHandleToKeyNameMap.TryGetValue(keyHandle, out keyName)) {
+                    registryHandleToKeyNameMap.Remove(keyHandle);
+                }
+                output.Write(data.TimeStampRelativeMSec, data.ProcessID, data.ThreadID,
+                    data.EventName, $"'{keyName}' (0x{keyHandle:X})");
+            }
+        }
+
         private void HandleRegistryTraceData(RegistryTraceData data)
         {
             if (data.ProcessID == pid) {
-                //ulong keyHandle = data.KeyHandle;
-                //string keyName;
-                //if (!registryHandleToKeyNameMap.TryGetValue(keyHandle, out keyName)) {
-                //    keyName = data.KeyName;
-                //    if (keyName != null) {
-                //        registryHandleToKeyNameMap.Add(keyHandle, keyName);
-                //    }
-                //} 
-                output.Write(data.TimeStampRelativeMSec, data.ProcessID, data.ThreadID, 
-                    data.EventName, $"'{data.KeyName}' '{data.ValueName}' (0x{data.KeyHandle:X})");
+                ulong keyHandle = data.KeyHandle;
+                string valueName = data.ValueName;
+                string keyName;
+                if (keyHandle != 0) {
+                    if (!registryHandleToKeyNameMap.TryGetValue(keyHandle, out keyName)) {
+                        keyName = data.KeyName;
+                        if (keyName != null) {
+                            registryHandleToKeyNameMap.Add(keyHandle, keyName);
+                        }
+                    }
+                } else {
+                    keyName = data.KeyName;
+                }
+                string value;
+                if (keyName != null && valueName != null) {
+                    value = $"{keyName}\\{valueName}";
+                } else if (keyName != null) {
+                    value = keyName;
+                } else {
+                    value = valueName;
+                }
+                output.Write(data.TimeStampRelativeMSec, data.ProcessID, data.ThreadID,
+                    data.EventName, $"'{data.KeyName}{data.ValueName}' (0x{keyHandle:X})");
             }
         }
     }
