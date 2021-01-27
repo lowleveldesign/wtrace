c6ccdcd1310ad59dfc6ca31d11c2d58f44de4ac7
Sebastian Solnica <ssolnica@gmail.com>
2021-01-27 20:31:34 +0100

    Updated PS update version script

--- scripts/Update-AssemblyInfoVersionFiles.ps1
+++ scripts/Update-AssemblyInfoVersionFiles.ps1
@@ -1,20 +1,21 @@
 function Update-AssemblyInfoVersionFiles ([string]$versionIdentifier)
 {
+  $local:srcPath = $pwd
   $today = [DateTime]::Today
   $local:buildNumber = "{0:yy}{1}.{2}" -f $today,$today.DayOfYear,($env:GITHUB_RUN_NUMBER % [int16]::MaxValue)
 
-  Write-Host "Executing Update-AssemblyInfoVersionFiles in path $pwd for build $buildNumber"
+  Write-Host "Executing Update-AssemblyInfoVersionFiles in path $srcPath for build $buildNumber"
 
-  foreach ($file in $(Get-ChildItem $pwd AssemblyInfo.cs -recurse))
+  foreach ($file in $(Get-ChildItem $srcPath *.csproj -recurse))
   {
-    $local:r = [regex]"$versionIdentifier\(""([0-9]+\.[0-9]+)(\.([0-9]+|\*))+""\)"
+    $local:r = [regex]"<$versionIdentifier>([0-9]+\.[0-9]+)(\.([0-9]+|\*))+"
     $local:assemblyVersion = "0.0.0.0"
     #version replacements
     (Get-Content -Encoding utf8 $file.FullName) | % {
       $m = $r.Matches($_)
       if ($m -and $m.Success) {
         $assemblyVersion = "$($m.Groups[1].Value).$buildNumber"
-        $local:s = $r.Replace($_, "$versionIdentifier(""`$1.$buildNumber"")")
+        $local:s = $r.Replace($_, "<$versionIdentifier>`$1.$buildNumber")
         Write-Host "Change version to $s"
         $s
       } else {
@@ -24,5 +25,5 @@ function Update-AssemblyInfoVersionFiles ([string]$versionIdentifier)
   }
 }
 
-Update-AssemblyInfoVersionFiles "AssemblyFileVersion" -Verbose
+Update-AssemblyInfoVersionFiles "FileVersion" -Verbose
 Update-AssemblyInfoVersionFiles "AssemblyVersion" -Verbose
